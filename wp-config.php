3670fd812d96b988e3cd0098f584b9b3c2ee7bc6
gobinath.m <gobinath.m>
2019-03-11 18:38:34 +0530

    db host change

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'b67b6556a385e7' );
 define( 'DB_PASSWORD', 'b67b6556a385e7' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'us-cdbr-iron-east-03.cleardb.net/heroku_c51d0b47d88b0cc?reconnect=true' );
+define( 'DB_HOST', 'us-cdbr-iron-east-03.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
